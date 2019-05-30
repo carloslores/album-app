ffae8a0e0b0f80933d01a4f506a94ab1348c71f8
Carlos Eduardo Lores Hernández <carloseduardoloreshernandez@MacBook-Pro-de-Carlos-2.local>
2019-05-30 13:50:11 +0200

    carousel done

--- src/components/AlbumContainer.js
+++ src/components/AlbumContainer.js
@@ -18,15 +18,11 @@ class AlbumContainer extends Component {
     }
 
     render(){
-        // let copy = this.props.album.album
-        // let half = Math.ceil(copy.length / 60)
-        // let part = copy.splice(0, half)
-        let matchId = this.props.album.album.filter(album=>album.albumId === Number(this.props.match.params.albumid))
-        // console.log(this.props.album.album[0])
-        // console.log(this.props.album.album.filter(album=>album.albumId === Number(this.props.match.params.albumid)))
-        // console.log(typeof this.props.match.params.albumid)
+       
+        let matchId = this.props.album.album.filter(album=>album.albumId === +this.props.match.params.albumid)
+   
         console.log(Array.isArray(matchId))
-       // console.log(this.props.album.album.map(al=>console.log(al)))
+  
         return(
             <div className="col-sm-10 col-md-12 col-lg-12 album">
                 <div className="row">
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,87 @@
+
+// import React from 'react';
+// import Carousel from 'react-bootstrap/Carousel'
+
+
+
+// const Slider = () =>{
+//     return(
+//         <main className="slider">
+//   <Carousel>
+//   <Carousel.Item>
+//     <img
+     
+//       className="d-block"
+//       src="http://res.cloudinary.com/dqphzmuq1/image/upload/v1556835271/samples/paintings/photo-1497534547324-0ebb3f052e88.jpg"
+//       alt="First slide"
+//     />
+//     <Carousel.Caption>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+//   <Carousel.Item className="slider">
+//     <img
+//       className="d-block"
+//       src="http://res.cloudinary.com/dqphzmuq1/image/upload/v1556835128/samples/paintings/photo-1503220178855-e31ec372b8ad.jpg"
+//       alt="Third slide"
+//     />
+   
+//     <Carousel.Caption>
+      
+//     </Carousel.Caption>
+//   </Carousel.Item>
+//   <Carousel.Item>
+//     <img
+//       className="d-block"
+//       src="http://res.cloudinary.com/dqphzmuq1/image/upload/v1556818773/paintings/photo-1503220178855-e31ec372b8ad.jpg"
+//       alt="Third slide"
+//     />
+
+//     <Carousel.Caption>
+     
+//     </Carousel.Caption>
+//   </Carousel.Item>
+// </Carousel>
+//         </main>
+ 
+//     )
+
+
+
+// }
+
+// export default Slider
+
 import React from "react"
 
 
-import { MDBCarousel, MDBCarouselCaption, MDBCarouselInner, MDBCarouselItem, MDBView, MDBMask, MDBContainer } from
+import { MDBCarousel,  MDBCarouselInner, MDBCarouselItem, MDBView,  } from
 "mdbreact";
 
 const Home  = () => {
   return (
-    <main>
-      <MDBCarousel
+    <main className="home">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img className="carousel-image" src="https://images.unsplash.com/photo-1511367734837-f2956f0d8020?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1388&q=80" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img className="carousel-image" src="https://images.unsplash.com/photo-1488841714725-bb4c32d1ac94?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1432&q=80" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img className="carousel-image" src="https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+      {/* <MDBCarousel
         activeItem={1}
         length={3}
         showControls={true}
@@ -43,7 +117,7 @@ const Home  = () => {
             </MDBView>
           </MDBCarouselItem>
         </MDBCarouselInner>
-      </MDBCarousel>
+      </MDBCarousel> */}
       </main>
  
   );
